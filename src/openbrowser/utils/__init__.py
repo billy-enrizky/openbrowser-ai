b78a465e78d1b75ba3173c69a7a97c0183d3ebf7
billy-enrizky <billy.suharno@mail.utoronto.ca>
2025-12-26 21:51:14 -0500

    refactor(config,cli): add flexible config loader and signal handler utilities

---
+++ src/openbrowser/utils/__init__.py
@@ -0,0 +1,9 @@
+"""Utility modules for openbrowser."""
+
+from src.openbrowser.utils.signal_handler import SignalHandler, AsyncSignalHandler
+
+__all__ = [
+    "SignalHandler",
+    "AsyncSignalHandler",
+]
+
